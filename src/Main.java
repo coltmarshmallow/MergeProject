61fe22eff97b139e2954b947a4a09bcaf3f75d14
1301657 <1301657@RG41111.rgu.ac.uk>
2015-03-13 11:01:53 +0000

    done

--- src/Main.java
+++ src/Main.java
@@ -13,23 +13,16 @@ import static jdk.nashorn.internal.objects.NativeMath.min;
  */
 public class Main {
     
+    // count variables intialised
     private static int comparisons = 0;
     private static int copies = 0;
     private static int recursiveCalls = 0;
-    /**
-     * @param a1
-     * @param left1
-     * @param right1
-     * @param left2
-     * @param right2
-     * @param a2
-     * @param left
-     * @param a
-     */
+    
     
 
     private static int []a;
     public static void main(String[] args){
+        //make array and start loop for sorting 1000 times
         a = getArray();
         printArray(a);
         for (int i =0; i < 1000; i++){
@@ -40,16 +33,22 @@ public class Main {
     }
     
     public static void sort(){
+        //create temporary array and start recursive method with left being the start and right being the end of the array
         int []tempArray = new int[a.length];
         Recursive(tempArray,0,a.length-1);
     }
     
     public static void sort2(){
+        ////create temporary array and start iterative method with left being the start and right being the end of the array
         int []tempArray = new int[a.length];
         Iterative(tempArray,0,a.length-1);
     }
     
     public static void Recursive(int []tempArray, int lowerIndex, int upperIndex){
+       // set up condition left < right
+       // then use call the recursive method
+       //first for section split on the start (left hand side) then the right hand side
+       //followed by the merge and since left != right must loop again
         if(lowerIndex != upperIndex){
             int mid = (lowerIndex+upperIndex)/2;
             Recursive(tempArray, lowerIndex, mid);
@@ -61,8 +60,10 @@ public class Main {
     }
     
     public static void Iterative(int[] tempArray, int lowerIndex, int upperIndex){
+        //set size to size of array in getArray otherwise get out of bounds error
         int size = 50;
         int[] arrayb = new int[size];
+        //loop to copy first array into a second array
         for (int i = 0; i < arrayb.length; i++){
             arrayb[i] = a[i];
             copies ++;
@@ -73,12 +74,15 @@ public class Main {
             copies ++;    
         }
         
+        //loop within loop mergeing  useing the temporary array and math sums to callculate the limits
+        //loop increment is ++ instead of pseudo code given becuase in this implementation it was found to work
         for (int i = 1; i < a.length; i ++){
             for (int j = 0; j < a.length; j ++){
                 merge(tempArray, Math.min(j+1, a.length)-1,(j+1), Math.min(j + 2*i, a.length)-1);
             }
         }  
         
+        //coping sorted array to original
         for (int i = 0; i < tempArray.length; i++){
              tempArray[i] = a[i];
              copies ++;
@@ -87,6 +91,7 @@ public class Main {
     }
     
     public static void merge(int []tempArray,int lowerIndexCursor,int higherIndex,int upperIndex){
+        //an algorythm to merge which uses a lot of condition statements and  and copies the fragements based on the index in the array after compared
         int tempIndex=0;
         int lowerIndex = lowerIndexCursor;
         int midIndex = higherIndex-1;
@@ -122,6 +127,7 @@ public class Main {
     }
     
     public static void printArray(int []array){
+        //print method which uses a loop to print the array and print the average count varibles
         for(int i : array){
             System.out.print(i+", ");
         }
@@ -132,6 +138,7 @@ public class Main {
     }
     
     public static int[] getArray(){
+        //set array size then loop to fill array with random number generator
         int size= 5;
         int[] array = new int[size];
         int item = 0;
